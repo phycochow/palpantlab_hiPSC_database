a93ee6ce5d735ec0738586bdae26ac4da4b8522d
Chris Siu Yeung Chow <siuyeung.chow@uqconnect.edu.au>
2024-09-17 21:12:33 +1000

    Removed source code

--- site_source/make_favicon.R
+++
@@ -1,9 +0,0 @@
-# create simply png of h2g text
-# converted to favicon by:
-# http://www.favicomatic.com/
-
-png("~/Downloads/h2_icon.png",height=620,width=620,units="px",res=300)
-par(mar=c(0,0,0,0))
-plot(0,0,col="white",bty="n",xlab="",ylab="",xaxt='n',yaxt='n',ann=F)
-text(x=0,y=0,labels=expression(italic(h[g]^2)),col="darkblue",cex=8)
-dev.off()
